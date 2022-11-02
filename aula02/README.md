f6b19858b88e550d449ec0b2a6248041aa5bae77
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-11-02 16:54:26 -0300

    Configurando server e front-end web

---
+++ aula02/README.md
@@ -0,0 +1,5 @@
+# Criando rotas e acesso
+## Anotações
+- Utilizando seeds com Prims
+- [Zod](https://github.com/colinhacks/zod)
+- S[hort Unique id](https://www.npmjs.com/package/short-unique-id)
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import Fastify from "fastify";
 import {PrismaClient} from '@prisma/client';
 import cors from "@fastify/cors";
+import {z} from 'zod';
+import ShortUniqueId from "short-unique-id";
 
 const prisma = new PrismaClient({
 	log: ['query']
@@ -26,6 +28,47 @@ async function bootstrap() {
 
 		return {count: count}
 	});
+
+	/**
+	 * Rota para contabilizar palpites.
+	 */
+	fastify.get('/guesses/count', async () => {
+		const count = await prisma.guess.count();
+
+		return {count: count}
+	});
+
+	/**
+	 * Rota para contabilizar usuários.
+	 */
+	fastify.get('/users/count', async () => {
+		const count = await prisma.user.count();
+
+		return {count: count}
+	});
+
+	/**
+	 * Criando um bolão.
+	 */
+	fastify.post('/pools', async (request, response) => {
+		const createPoolBody = z.object({
+			title: z.string()
+		})
+
+		const {title} = createPoolBody.parse(request.body);
+		const generate = new ShortUniqueId({length: 6});
+		const code = String(generate()).toLowerCase();
+
+		await prisma.pool.create({
+			data: {
+				title,
+				code
+			}
+		})
+
+		response.status(201).send({code});
+	});
+
 	await fastify.listen({port: 3333, host: '0.0.0.0'});
 
 }
