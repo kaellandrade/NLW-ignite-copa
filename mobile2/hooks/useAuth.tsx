3f96645f3a6a8a28775fc5dbf43b3b0723190a2f
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-11-04 20:44:25 -0300

    readme

--- mobile2/hooks/useAuth.tsx
+++
@@ -1,6 +0,0 @@
-import {useContext} from "react";
-import {AuthContext, AuthContextDataProps} from "../context/AuthContext";
-
-export function useAuth():AuthContextDataProps {
-	return useContext(AuthContext);
-}
