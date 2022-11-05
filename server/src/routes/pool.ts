2cb1f997648efdc8b87b93ccfe79fba776a42811
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-11-04 22:57:09 -0300

    Criando minhas rotas

---
+++ server/src/routes/pool.ts
@@ -0,0 +1,112 @@
+import {FastifyInstance} from "fastify";
+import {prisma} from "../lib/prisma";
+import {z} from "zod";
+import ShortUniqueId from "short-unique-id";
+import {authenticate} from "../plugin/authenticate";
+
+/**
+ * Rotas para o bolão.
+ * @param fastify
+ */
+export async function pollRoutes(fastify:FastifyInstance){
+	/**
+	 * Rota para contabilizar bolões.
+	 */
+	fastify.get('/pools/count', async () => {
+		const count = await prisma.pool.count();
+		return {count: count}
+	});
+
+	/**
+	 * Criando um bolão.
+	 */
+	fastify.post('/pools', async (request, response) => {
+		const createPoolBody = z.object({
+			title: z.string()
+		})
+
+		const {title} = createPoolBody.parse(request.body);
+		const generate = new ShortUniqueId({length: 6});
+		const code = String(generate()).toLowerCase();
+
+		try {
+			await request.jwtVerify();
+			await prisma.pool.create({
+				data: {
+					title,
+					code,
+					ownerId: request.user.sub,
+					participants: {
+						create: {
+							userId:request.user.sub
+						}
+					}
+				}
+			});
+		}catch (e) {
+			console.log(e);
+			await prisma.pool.create({
+				data: {
+					title,
+					code
+				}
+			});
+		}
+
+
+		response.status(201).send({code});
+	});
+
+	fastify.post('/pools/:id/join', {
+		onRequest:[authenticate]
+	},async (request, response)=>{
+		const joinPoolBody = z.object({
+			code: z.string()
+		});
+		const {code} = joinPoolBody.parse(request.body);
+
+		const pool = await prisma.pool.findUnique({
+			where: {
+				code,
+			},
+			include : {
+				participants: {
+					where: {
+						userId: request.user.sub
+					}
+				}
+			}
+		});
+		if(!pool) {
+			return response.status(400).send({
+				message: 'Bolão não encontrado'
+			});
+		}
+		if(pool.participants.length > 0) {
+			return response.status(400).send({
+				message: 'Você já faz parte desse bolão!'
+			});
+		}
+
+		// Caso bolão não tenha dono o primeiro entrar é o dono.
+		if(!pool.ownerId) {
+			await prisma.pool.update({
+				where: {
+					id: pool.id
+				},
+				data: {
+					ownerId: request.user.sub
+				}
+			});
+		}
+
+		await prisma.participant.create({
+			data: {
+				poolId: pool.id,
+				userId: request.user.sub
+			}
+		});
+		return response.status(201).send();
+
+	})
+}
