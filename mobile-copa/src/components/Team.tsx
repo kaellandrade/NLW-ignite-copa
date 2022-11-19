0d615084c8cf83d3530aff311e04dff490b0b6ed
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-11-19 18:36:08 -0300

    Configurando tamanho das bandeiras.

--- mobile-copa/src/components/Team.tsx
+++ mobile-copa/src/components/Team.tsx
@@ -7,23 +7,27 @@ interface Props {
 	code: string;
 	position: 'left' | 'right';
 	onChangeText: (value: string) => void;
+	openToGuess: boolean;
 }
 
-export function Team({code, position, onChangeText}: Props) {
+export function Team({code, position, onChangeText, openToGuess = true}: Props) {
+	const FLAG_SIZE = 36;
 	return (
 		<HStack alignItems="center">
-			{position === 'left' && <CountryFlag isoCode={code} size={25} style={{marginRight: 12}}/>}
+			{position === 'left' && <CountryFlag isoCode={code} size={FLAG_SIZE} style={{marginRight: 12}}/>}
 
-			<Input
-				w={10}
-				h={9}
-				textAlign="center"
-				fontSize="xs"
-				keyboardType="numeric"
-				onChangeText={onChangeText}
-			/>
+			{openToGuess ?
+				<Input
+					w={10}
+					h={9}
+					textAlign="center"
+					fontSize="xs"
+					keyboardType="numeric"
+					onChangeText={onChangeText}
+				/> : null
+			}
 
-			{position === 'right' && <CountryFlag isoCode={code} size={25} style={{marginLeft: 12}}/>}
+			{position === 'right' && <CountryFlag isoCode={code} size={FLAG_SIZE} style={{marginLeft: 12}}/>}
 		</HStack>
 	);
 }
