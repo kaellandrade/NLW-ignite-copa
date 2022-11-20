116734fff916706e8fd6bc7a215567063f4b6c4f
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-11-19 22:17:17 -0300

    Ajustes configurações

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,9 +2,13 @@ import '../styles/global.css';declare module "*.svg" {
   import React from 'react';
   import { SvgProps } from "react-native-svg";
   const content: React.FC<SvgProps>;
-  export default content;
 }
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+	  <>
+		  <title>NLW Copa 2022</title>
+		<Component {...pageProps} />
+	  </>
+	  )
 }
--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
+				<link rel="shortcut icon" href="/images/favicon.ico" />
 				<link rel="preconnect" href="https://fonts.googleapis.com"/>
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
 				<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700;900&family=Roboto:wght@400;700&display=swap" rel="stylesheet"/>
