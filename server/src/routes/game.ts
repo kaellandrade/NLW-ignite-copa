c748608d7a3858909e39d7499fbf156fa307d77b
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2022-11-20 13:26:56 -0300

    ordenando os jogos por datas

--- server/src/routes/game.ts
+++ server/src/routes/game.ts
@@ -15,7 +15,7 @@ export async function gameRoutes(fastify: FastifyInstance) {
 		const {id} = getPoolParams.parse(request.params);
 		const games = await prisma.game.findMany({
 			orderBy:{
-				date: 'desc'
+				date: 'asc'
 			},
 			include:{
 				guesses: {
